0c97c4d7373277de4d79d94388988b4e5c1f28c9
huesuk12 <huesuk4720@naver.com>
2026-08-30 13:16:39 +0900

    Initial Spring Boot project setup with H2 for local dev

---
+++ src/main/java/com/hoeseok/yearly_goal_tracker/YearlyGoalTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.hoeseok.yearly_goal_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YearlyGoalTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YearlyGoalTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoeseok/yearly_goal_tracker/YearlyGoalTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoeseok.yearly_goal_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YearlyGoalTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
